1528fcced4d4640761f7a6f8d7510ca908e708cf
alesstongwen <alesstongwen@gmail.com>
2023-12-02 22:58:08 -0800

    second

--- app.js
+++ app.js
@@ -4,49 +4,47 @@ const myMovieList = document.querySelector("ul");
 const movieHistoryContainer = document.getElementById("tableContainer");
 let movieHistory = [];
 
-// Example of a simple function that clears the input after a user types something in
-function clearInput() {
-  inp.value = "";
-}
+const movieHistoryCard = document.getElementById("movieHistoryCard");
 
-function validateForm(userInput) {
+// Create the table structure
+const tableContainer = document.createElement("div");
+tableContainer.id = "tableContainer";
+const table = document.createElement("table");
+table.className = "table";
+const tableHead = document.createElement("thead");
+const tableHeadRow = document.createElement("tr");
+["Movie Name", "Watched Count"].forEach((headerText) => {
+  const th = document.createElement("th");
+  th.textContent = headerText;
+  tableHeadRow.appendChild(th);
+});
+tableHead.appendChild(tableHeadRow);
+const tableBody = document.createElement("tbody");
+tableBody.id = "movieHistoryTable";
+table.appendChild(tableHead);
+table.appendChild(tableBody);
+tableContainer.appendChild(table);
+
+// Append the table to the movie history card
+movieHistoryCard.appendChild(tableContainer);
+
+// Example of a simple function that clears the input after a user types something in
+const clearInput = () => (inp.value = "");
+
+const validateForm = (userInput) => {
   if (userInput === "") {
     alert("Please enter a movie name!");
     return false;
   }
   return true;
-}
-
-function createMovieHistoryTable() {
-  const movieHistoryCard = document.getElementById("movieHistoryCard");
-
-  // Create the table structure
-  const tableContainer = document.createElement("div");
-  tableContainer.id = "tableContainer";
-  const table = document.createElement("table");
-  table.className = "table";
-  const tableHead = document.createElement("thead");
-  const tableHeadRow = document.createElement("tr");
-  ["Movie Name", "Watched Count"].forEach((headerText) => {
-    const th = document.createElement("th");
-    th.textContent = headerText;
-    tableHeadRow.appendChild(th);
-  });
-  tableHead.appendChild(tableHeadRow);
-  const tableBody = document.createElement("tbody");
-  tableBody.id = "movieHistoryTable";
-  table.appendChild(tableHead);
-  table.appendChild(tableBody);
-  tableContainer.appendChild(table);
-
-  // Append the table to the movie history card
-  movieHistoryCard.appendChild(tableContainer);
-}
+};
 
 // Call the function to create the movie history table
 createMovieHistoryTable();
 
-function generateTable(table, data) {
+const movieHistoryContent = () => {};
+
+const generateTable = (table, data) => {
   const rows = data.map((element) => {
     const row = table.insertRow();
     Object.values(element).forEach((value) => {
@@ -57,9 +55,9 @@ function generateTable(table, data) {
     return row;
   });
   return rows;
-}
+};
 
-function movieCount(movie) {
+const movieCount = (movie) => {
   const index = movieHistory.findIndex((item) => item.movie === movie);
   if (index !== -1) {
     movieHistory[index].count++;
@@ -68,14 +66,14 @@ function movieCount(movie) {
   }
   movieHistoryContainer.innerHTML = "";
   generateTable(movieHistoryContainer, movieHistory);
-}
-function clearMovies() {
+};
+const clearMovies = () => {
   // To delete all children of the <ul></ul> (meaning all <li>'s)..we can wipe out the <ul>'s innerHTML
   myMovieList.innerHTML = "";
-}
+};
 
 // This function is executed when the user clicks [ADD MOVIE] button.
-function addMovie() {
+const addMovie = () => {
   // Step 1: Get value of input
   let userTypedText = inp.value;
   if (!validateForm(userTypedText)) {
@@ -97,4 +95,4 @@ function addMovie() {
   movieCount(userTypedText);
   // Step 6: Call the clearInput function to clear the input field
   clearInput();
-}
+};
